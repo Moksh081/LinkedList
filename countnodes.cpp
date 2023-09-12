6f67e9ac975513b76b0df8f14a677c86d38bb71c
Moksh Sharma <109467184+Moksh081@users.noreply.github.com>
2023-09-12 10:13:10 +0530

    Add files via upload

---
+++ countnodes.cpp
@@ -0,0 +1,41 @@
+//count nodes linked list
+
+/*
+#include<iostream>
+using namespace std;
+class node{
+	public:
+	int data;
+	node *next;
+};
+int main(){
+	int A[] = {3, 5, 7, 10, 15};
+	node *first;
+	node *temp;
+	node *last;
+	
+	first = new node;
+	first->data = A[0];
+	first->next = NULL;
+	last = first;
+	
+	for(int i=1; i<5;i++){
+		temp = new node;
+		temp->data = A[i];
+		temp->next = NULL;
+		last->next = temp;
+		last = temp;
+	}
+	
+	node *p = first;
+	int count =0;
+	while(p!=NULL){
+		count++;
+        p = p->next;
+	}
+	cout<<count;
+	return 0;
+}
+
+*/
+
