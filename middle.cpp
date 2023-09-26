991146d40ba8f2b0c0e9f8e1bb2ca34e27c5c3f7
Moksh Sharma <109467184+Moksh081@users.noreply.github.com>
2023-09-26 07:19:07 +0530

    Add files via upload

---
+++ middle.cpp
@@ -0,0 +1,42 @@
+//middle 
+ 
+/*
+  #include<iostream>
+using namespace std;
+class node{
+	public:
+	int data;
+	node *next;
+};
+int main(){
+	int A[] = {3, 5, 7, 10, 8, 15};
+	node *first;
+	node *temp;
+	node *last;
+	
+	first = new node;
+	first->data = A[0];
+	first->next = NULL;
+	last = first;
+	
+	for(int i=1; i<5;i++){
+		temp = new node;
+		temp->data = A[i];
+		temp->next = NULL;
+		last->next = temp;
+		last = temp;
+	}
+	node* p = first;
+	node* q = first;
+	while(q){
+		q = q->next;
+		if(q){
+			q = q->next;
+			p = p->next;
+		}
+	}
+	cout<<p->data;
+	return 0;
+}
+ */
+ 
